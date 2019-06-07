1700b6b316acf966ec627502b7f19aebfefcb950
rusa01 <rutuja.saptarshi@dfki.de>
2019-06-07 13:47:20 +0200

    init commit

---
+++ IoT_Pong_game/receive/Receive.java
@@ -0,0 +1,21 @@
+package receive;
+
+public interface Receive {
+	
+	public int ultrasonic_1();
+	
+	public int ultrasonic_2();
+	
+	public int ultrasonic_3();
+	
+	public int ultrasonic_4();
+	
+	public int ball_speed();
+	
+	public int ambient_light();
+	
+	public void start(String port_name);
+	
+	public void close();
+
+}
---
+++ IoT_Pong_game/receive/SerialReceive.java
@@ -0,0 +1,101 @@
+package receive;
+
+import java.util.concurrent.TimeUnit;
+
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortException;
+
+public class SerialReceive implements Receive {
+
+	private SerialPort serialPort;
+	
+	@Override
+	public int ultrasonic_1() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int ultrasonic_2() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int ultrasonic_3() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int ultrasonic_4() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int ball_speed() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int ambient_light() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+	@Override
+	public void start(String port_name) {
+		// TODO Auto-generated method stub
+
+		serialPort = new SerialPort("/dev/ttyACM0");
+		
+		serialPort.openPort();
+
+	    serialPort.setParams(SerialPort.BAUDRATE_9600,
+	                         SerialPort.DATABITS_8,
+	                         SerialPort.STOPBITS_1,
+	                         SerialPort.PARITY_NONE);
+
+	    serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN | 
+	    							SerialPort.FLOWCONTROL_RTSCTS_OUT);
+	    	
+	    serialPort.addEventListener(new PortReader(), SerialPort.MASK_RXCHAR);
+	    		
+	
+	}
+	
+	protected static class SerialPortReader implements SerialPortEventListener{
+		
+	    @Override
+	    public void serialEvent(SerialPortEvent event) {
+	        if(event.isRXCHAR() && event.getEventValue() > 0) {
+	            try {
+	            	//System.out.println("event value" + event.getEventValue() );
+	            	TimeUnit.MILLISECONDS.sleep(10);
+	                String receivedData = serialPort.readString();
+	                System.out.println("Received response: " + receivedData);
+	            }
+	            catch (SerialPortException ex) {
+	                System.out.println("Error in receiving string from COM-port: " + ex);
+	            } catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        }
+	    }
+
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+		serialPort.close();
+	}
+
+
+
+}
