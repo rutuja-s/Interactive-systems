f360a4eb3092eabbf0ba882fb60cb319295ad83c
Radon95 <s9togehl@stud.uni-saarland.de>
2019-06-13 13:18:01 +0200

    added port file

--- IoT_Pong_game/pong/Pong.java
+++ IoT_Pong_game/pong/Pong.java
@@ -9,6 +9,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Random;
 
 import javax.swing.JFrame;
@@ -64,8 +69,23 @@ public class Pong implements ActionListener, KeyListener
 
 		timer.start();
 		
-		receive = new SerialReceive();
-		receive.start("/dev/ttyACM0");
+		File file = new File("./port.txt");
+		String port = null;
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			port = br.readLine();
+		} catch (FileNotFoundException e) {
+			System.out.println("port.txt does not exist");
+			System.exit(0);
+		} catch (IOException e) {
+			System.out.println("couldn't read port.txt");
+		}
+		if (port == null) {
+			System.out.println("no port defined");
+		} else {
+			receive = new SerialReceive();
+			receive.start(port);
+		}
 	}
 
 	public void start()
